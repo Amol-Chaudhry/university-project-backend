9c0fe54a9b4b53c2343196d134038ec41b7ac819
Amol-Chaudhry <amolchaudhry5@gmail.com>
2023-08-30 13:07:26 +0100

    Validation updated

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,7 +5,11 @@ exports.userAuthInputValidation = [
     check("firstName").trim().not().isEmpty().withMessage("Firstname Blank!").isLength({max: 150}).withMessage("Firstname should contain a maximum of 150 characters!"),
     check("lastName").trim().not().isEmpty().withMessage("Lastname Blank!").isLength({max: 150}).withMessage("Lastname should contain a maximum of 150 characters!"),
     check("userName").trim().not().isEmpty().withMessage("userName Blank!").isLength({min:2, max: 150}).withMessage("Username length must be between 2 and 150 characters!"),
-    check("email").isEmail().withMessage("Invalid email!"),
+    check("email").isEmail().withMessage("Invalid email!").custom((value, { req }) => {
+        if (!value.endsWith("@student.bham.ac.uk")) throw new Error("Please enter your student email.");
+        
+        return true;
+    }),
     check("password").trim().not().isEmpty().withMessage("Empty password!").isLength({ min: 6}).withMessage("Length of password must be minimum 6 characters!"),
     check("courseTitle").trim().not().isEmpty().withMessage("Empty Course Title!")
 ]
